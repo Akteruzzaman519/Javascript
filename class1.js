258205da2dc262c0f881a921df35a42d7978d23e
akteruzzaman51 <mdakteruzzaman519@gmail.com>
2020-09-01 22:49:32 +0600

    initial commit

---
+++ class1.js
@@ -0,0 +1,8 @@
+console.log( typeof 2)
+console.log(typeof false)
+console.log(typeof"name")
+console.log(typeof{Name:"address"})
+console.log( typeof ["address","contract"])
+console.table({name:"Akteruzzaman",
+Address:"Khoksha Mollikpara",Contract:"Dhaka"});
+console.log(["Akteruzzaman","suroz","ali"]);
