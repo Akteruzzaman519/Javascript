9a4919acf9a015e0d87bc711250f504fa58b9492
akteruzzaman51 <mdakteruzzaman519@gmail.com>
2020-09-02 20:39:10 +0600

    variable and browser input system and  show in

---
+++ class1.js
@@ -0,0 +1,11 @@
+console.log(4)
+
+var str="hello how are you what are you doing now in this time to go ";
+
+console.log(str.lastIndexOf(""))
+console.log(str.indexOf("you",15))
+
+var str = "Apple, Banana, Kiwi";
+var res =new Date();
+console.log(res)
+console.log(res.getTime())
--- day2.js
+++ day2.js
@@ -1,2 +1,18 @@
 // Variables
+var number=0878;
+var faction=9.988;
+var name="Md. Akteruzzaman"
+var names=["Akteruzzaman","Suroz","Rasel","Foroz"]
+var person={name:"Suroz",Address:"Dhaka",Contract:"01940533029"}
+console.log(number)
+name=true
+console.log(name)
 // Browser IO JavaScript 
+var name=prompt("Enter your name ","");
+document.getElementById("name").innerHTML=name;
+var address=prompt("Enter your address ","");
+document.getElementById("address").innerHTML=address;
+var contract=prompt("Enter your contract ","");
+document.getElementById("contract").innerHTML=contract;
+var salary=prompt("Enter your salary ","");
+document.getElementById("salary").innerHTML=salary;
