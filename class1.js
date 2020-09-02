2fbb105d4abf0cd477247c52f16062ba34544dda
akteruzzaman51 <mdakteruzzaman519@gmail.com>
2020-09-02 19:48:56 +0600

    file delete class 1

--- class1.js
+++
@@ -1,8 +0,0 @@
-console.log( typeof 2)
-console.log(typeof false)
-console.log(typeof"name")
-console.log(typeof{Name:"address"})
-console.log( typeof ["address","contract"])
-console.table({name:"Akteruzzaman",
-Address:"Khoksha Mollikpara",Contract:"Dhaka"});
-console.log(["Akteruzzaman","suroz","ali"]);
